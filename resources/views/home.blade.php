70a3e86dcf96c9371cf48ded23d2fc335bbd0bfe
Enzo Barlotta <ebarlotta@yahoo.com.ar>
2023-09-05 10:39:57 -0300

    Segundo commit de prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,8 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <h2>FELICITACIONES MASTER!!!</h2>
                 </div>
             </div>
         </div>
